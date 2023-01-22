a4825b8a9c9317f591c0e9fe3f47333568a9948e
nicola <nicola.posa@defstudio.it>
2023-01-22 14:50:27 +0100

    Add ServiceProvider

---
+++ src/RestoreServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DefStudio\Restore;
+
+use Illuminate\Support\ServiceProvider;
+
+class RestoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
